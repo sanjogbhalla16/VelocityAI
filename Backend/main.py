77e6ccaab574dbd8db16fa4a753d5359e40f7abf
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-03-06 18:51:45 +0530

    Changes in Backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class villanChatRequest(BaseModel):
+    villan_name: str
+    user_message:str
+    
+    
+    
 
 @app.get("/")
 async def root():
