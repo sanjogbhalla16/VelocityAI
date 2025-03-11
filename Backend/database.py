4554613b7f6117530c7c4fa5a3e343012b1c5de5
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-03-12 01:04:50 +0530

    Changes in Backend

--- Backend/database.py
+++
@@ -1,6 +0,0 @@
-import chromadb
-
-chromadb_client = chromadb.Client() #the client is used to connect to the database
-
-#create a collection
-collection = chromadb_client.create_collection(name="my_villan_collection")
